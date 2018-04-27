6a5c10aad691c7112d099de505bed2786f6b8380
thapgan <thapgan1010@gmail.com>
2018-04-27 15:33:08 +0700

    Updates

---
+++ tocbot.js
@@ -0,0 +1,7 @@
+tocbot.init({ tocSelector: '.js-toc',
+      // Where to grab the headings to build the table of contents.
+      contentSelector: '.js-toc-content',
+      // Which headings to grab inside of the contentSelector element.
+      headingSelector: 'h2, h3',
+      collapseDepth: 6
+});
